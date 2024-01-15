24ac740ed9b7ebcf08fdce875eb1818b16c5d1ae
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-01-15 11:43:48 +0530

    fixes

--- Student API/middleware/authentication.js
+++ Student API/middleware/authentication.js
@@ -6,14 +6,13 @@ const checkAuth = (req, res, next) => {
         return next();
     }
     const token = authHeader.split(' ')[1];
-
-    // if (!token) return next();
     try {
         const payload = jwt.verify(token, process.env.JWT_SECERT);
         req.user = { ...payload };
         next();
     } catch (err) {
         console.log(err);
+        next();
     }
 }
 
